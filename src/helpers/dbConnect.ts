d5a26ce07462eb3dd6d0a0770ae30e1b0da22290
karadenizemirr <karadenizemirr@gmail.com>
2023-07-15 02:56:27 +0300

    Update -1

--- src/helpers/dbConnect.ts
+++ src/helpers/dbConnect.ts
@@ -8,16 +8,34 @@ import { Guest } from "src/entity/guest.entity";
 import { CreditCard } from "src/entity/creditCard.entity";
 import { Secure } from "src/entity/secure.entity";
 
+// export const AppDataSource = new DataSource({
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "administrator",
+//     password: "123456",
+//     database: "cemileozel",
+//     entities: [User,House, Image, Appoinment, Guest, CreditCard,Secure],
+//     migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+//     synchronize: true,
+//     logging: true,
+//     subscribers: [],
+// })
+
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "administrator",
-    password: "123456",
-    database: "cemileozel",
+    host: "db-mysql-sfo3-87316-do-user-9457349-0.b.db.ondigitalocean.com",
+    port: 25060,
+    username: "doadmin",
+    password: "AVNS_p5LwzrJ8NFbk5IOrslc",
+    database: "defaultdb",
     entities: [User,House, Image, Appoinment, Guest, CreditCard,Secure],
     migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
     synchronize: true,
     logging: true,
     subscribers: [],
+    ssl: {
+        rejectUnauthorized: false,
+        sslmode: "require",
+    }
 })
